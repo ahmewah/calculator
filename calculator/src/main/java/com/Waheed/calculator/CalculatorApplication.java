60294b94dc00e31dfca24862b295af2964c0c7c3
ahmewah <ahmewah75@gmail.com>
2018-02-23 01:53:19 +0530

    added mvn files

---
+++ calculator/src/main/java/com/Waheed/calculator/CalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.Waheed.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+}
